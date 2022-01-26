8fa9631a46c82b483078b2dcdf97634c5a0acabb
Tkxiong89 <txiong401@gmail.com>
2022-01-25 18:04:27 -0600

    solved is array sorted problem

--- algorithmic_strategies_exercise/02_is_sorted.js
+++ algorithmic_strategies_exercise/02_is_sorted.js
@@ -1,8 +1,15 @@
 /* Write a method, is_sorted(arr), that accepts an array of numbers as an arg.
   The method should return true if the elements are in increasing order, false otherwise. */
 
+function isSorted(array) {
+    for (let i = 0; i < array.length; i++) {
+        if ( array[i] > array[i+1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
-
-is_sorted([1, 4, 10, 13, 15])     // => true
-is_sorted([1, 4, 10, 10, 13, 15])   // => true
-is_sorted([1, 2, 5, 3, 4 ])         // => false
+console.log(isSorted([1, 4, 10, 13, 15]));     // => true
+console.log(isSorted([1, 4, 10, 10, 13, 15]));   // => true
+console.log(isSorted([1, 2, 5, 3, 4 ]));        // => false
