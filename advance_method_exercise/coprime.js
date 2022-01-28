a7f76f66e48aa4ca12d6dbe7262f23b302818a63
Tkxiong89 <txiong401@gmail.com>
2022-01-27 23:05:08 -0600

    advance method exercise folder

---
+++ advance_method_exercise/coprime.js
@@ -0,0 +1,4 @@
+/** # Write a method, coprime(num_1, num_2), that accepts two numbers as args.
+# The method should return true if the only common divisor between the two numbers is 1.
+# The method should return false otherwise. For example coprime(25, 12) is true because
+# 1 is the only number that divides both 25 and 12. **/
